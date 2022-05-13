a469551e028abafab8e116306d070b39aaaf5bca
DafterWatch <marcosingefa@gmail.com>
2022-05-13 13:20:50 -0400

    s

--- src/app/components/menuproductos2/menuproductos2.component.ts
+++ src/app/components/menuproductos2/menuproductos2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VetServiceService } from 'src/app/services/vet-service.service';
 
 @Component({
   selector: 'app-menuproductos2',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Menuproductos2Component implements OnInit {
 
-  constructor() { }
-
+  constructor(private router:Router, private _productosServices:VetServiceService) { }
+  productos: any[] = [];
   ngOnInit(): void {
+    this.getEmpleados();
   }
-
+  getEmpleados(){
+    this._productosServices.getProductos().subscribe(data => {
+      this.productos = [];
+      data.forEach((element:any) => {
+        this.productos.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+    })
+  }
+  eliminarProducto(id: string){
+    this._productosServices.eliminarProducto(id).then(()=>{
+      console.log('Producto eliminado con exito');
+    }).catch(error => {
+      console.log(error);
+    })
+  }
 }
